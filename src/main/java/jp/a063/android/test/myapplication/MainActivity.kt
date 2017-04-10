b701e2b72bbf1d21f9b15564f088f4a23eeddec9
Goryudyuma <umaretekyoumade@gmail.com>
2017-04-10 18:48:31 +0900

    buttonの感知

--- src/main/java/jp/a063/android/test/myapplication/MainActivity.kt
+++ src/main/java/jp/a063/android/test/myapplication/MainActivity.kt
@@ -4,15 +4,22 @@ import android.app.Activity
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import jp.a063.android.test.myapplication.R.layout.activity_main
 import java.net.URL
 import java.nio.charset.Charset
 import java.util.regex.Pattern
 
-class MainActivity : Activity() {
+class MainActivity : Activity(),View.OnClickListener{
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button:Button=findViewById(R.id.button)as Button
+        button.setOnClickListener(this)
     }
     override fun onResume(){
         super.onResume()
@@ -28,5 +35,9 @@ class MainActivity : Activity() {
                 Toast.makeText(getApplication(),result?:"null",Toast.LENGTH_LONG).show()
             }
         }.execute()
+    }
+
+    override fun onClick(v: View?){
+        Toast.makeText(this,"Tapped",Toast.LENGTH_SHORT).show()
     }
 }
